cb8a0050db21fe0c75355e5203cdf5fa350b5613
Sardor <tojimuradovsardor25@gmail.com>
2021-09-25 17:35:21 +0500

    API davomi jamoa bilan
    API LINK va SAYT

--- api.js
+++ api.js
@@ -1,35 +1,37 @@
-const gitter1 = document.getElementById('gitter1')
-const appert2 = document.getElementById('appert2')
+// const gitter1 = document.getElementById('gitter1')
+// const appert2 = document.getElementById('appert2')
 
+// getWeatherData()
+// function getWeatherData() {
+//     navigator.geolocation.getCurrentPosition((success) => {
+//         let { latitude, longitude } = success.coords;
+//         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEYSUZ}`).then(res => res.json()).then(data => {
+//             console.log(data);
+//             showWeatherData(data);
+//         })
+//     })
+// }
 
-const API_KEYSUZ = '316b9736dbcafba7127d93281fa85cc0';
-
-getWeatherData()
-function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-        let { latitude, longitude } = success.coords;
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEYSUZ}`).then(res => res.json()).then(data => {
-            console.log(data);
-            showWeatherData(data);
-        })
-    })
-}
-
-function showWeatherData(data) {
-    let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
-    gitter1.innerHTML = `
-    <div class="api" id="appert2">${sunrise}</div>
-    `
-}
-
-
-
-
+// const API_KEYSUZ = '316b9736dbcafba7127d93281fa85cc0';
+// function showWeatherData(data) {
+//     let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
+//     gitter1.innerHTML = `
+//     <div class="api" id="appert2">${humidity}</div>
+//     `
+// }
 
 // fetch('https://jsonplaceholder.typicode.com/todos/1')
 //     .then(response => response.json())
 //     .then(json => console.log(json))
 
-
-// const kunlar = ['Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba', 'Yakshanba']
-// const oylar = ['Mart', 'Aprel', 'May', 'Iyun', 'Iyul', 'Avgust', 'Sentabr', 'Oktabr', 'Noyabr', 'Dekabr', 'Yanvar', 'Fevral']
+getWeatherData();
+function getWeatherData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    fetch(`http://worldtimeapi.org/api/timezone/Asia/Tashkent`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // showWeatherData(data);
+      });
+  });
+}
